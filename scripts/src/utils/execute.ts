73a4a717ed79903167a729d4fa18f966a19a94b5
Witchayut <skpepe134@gmail.com>
2021-12-07 13:50:12 +0700

    remove wallet 2

--- scripts/src/utils/execute.ts
+++ scripts/src/utils/execute.ts
@@ -5,7 +5,7 @@ import {
   StdFee,
 } from "@terra-money/terra.js";
 
-import { feeDenom, terra, wallet, wallet2 } from "../constant";
+import { feeDenom, terra, wallet } from "../constant";
 import { retry } from "./retry";
 
 export const execute = async (
@@ -16,25 +16,6 @@ export const execute = async (
   const tx = await wallet.createAndSignTx({
     msgs: [
       new MsgExecuteContract(wallet.key.accAddress, address, msgObj, coins),
-    ],
-    fee: new StdFee(2000000, "500000uusd"),
-  });
-
-  const result = await terra.tx.broadcast(tx);
-  if (result.raw_log.includes("failed")) {
-    throw result;
-  }
-  return result;
-};
-
-export const execute2 = async (
-  address: string,
-  msgObj: any,
-  coins?: Coins.Input
-) => {
-  const tx = await wallet2.createAndSignTx({
-    msgs: [
-      new MsgExecuteContract(wallet2.key.accAddress, address, msgObj, coins),
     ],
     fee: new StdFee(2000000, "500000uusd"),
   });
